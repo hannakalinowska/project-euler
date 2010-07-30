fa3465c4150a9386d10fd124bd0e856c40fdcdca
Hanna Kalinowska <hanna.kalinowska@gmail.com>
2010-07-30 21:53:25 +0100

    problem 3

---
+++ problem003.rb
@@ -0,0 +1,27 @@
+#! /usr/bin/ruby
+
+number = 600851475143
+root = (number ** 0.5).to_i
+max_lpf = 0
+
+def largest_prime_factor(number)
+  root = (number ** 0.5).to_i
+  root.downto(2) do |i|
+    if i.odd? || i == 2
+      if number % i == 0
+        return largest_prime_factor(number/i)
+      end
+    end
+  end
+  return number
+end
+
+root.downto(2) do |i|
+  if i.odd? || i == 2
+    if number % i == 0
+      lpf = largest_prime_factor(number/i)
+      max_lpf = lpf if max_lpf < lpf
+    end
+  end
+end
+puts max_lpf
