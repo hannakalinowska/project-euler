912f47c4ac54448f9599be6ff5f0fdbc0b8802e7
Hanna Kalinowska <hanna.kalinowska@gmail.com>
2010-07-31 11:01:46 +0100

    problem 7

---
+++ problem007.rb
@@ -0,0 +1,19 @@
+#! /usr/local/bin/ruby
+
+iterations = 10001
+prime_numbers = [2, 3, 5]
+number = 7
+
+while prime_numbers.length < iterations do
+  prime = true
+  prime_numbers.each do |prime_number|
+    prime = true
+    if number % prime_number == 0
+      prime = false
+      break
+    end
+  end
+  prime_numbers << number if prime 
+  number += 2
+end
+puts prime_numbers.last
