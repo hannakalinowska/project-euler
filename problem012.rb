f09745222c19208cdbc2ad6dfc7999e6ce94d897
Hanna Kalinowska <hanna.kalinowska@gmail.com>
2010-07-31 16:35:08 +0100

    problem 12

---
+++ problem012.rb
@@ -0,0 +1,20 @@
+#! /usr/bin/ruby
+require 'mathn'
+
+def divisor_count_for(number)
+  count = 0
+  (1 .. Math.sqrt(number).to_i).each do |i|
+    count += 2 if number % i == 0
+  end
+  count
+end
+
+number_divisors = 500
+
+number = 1
+current_integer = 2
+while (count = divisor_count_for(number)) <= number_divisors do
+  number += current_integer
+  current_integer += 1
+end
+puts number
