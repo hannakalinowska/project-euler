3ee741729ccd6da6a788914715c9f4292cb80679
Hanna Kalinowska <hanna.kalinowska@gmail.com>
2010-07-31 11:13:30 +0100

    problem 9

---
+++ problem009.rb
@@ -0,0 +1,11 @@
+#! /usr/local/bin/ruby
+
+(1 .. 1000).each do |a|
+  (a .. 1000).each do |b|
+    c = 1000 - a - b
+    if a**2 + b**2 == c**2
+      puts "#{a} * #{b} *  #{c} = #{a * b * c}"
+      exit
+    end
+  end
+end
