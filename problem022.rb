645596f194ca1e43aac4a9b68adf8e372fd82bd6
Hanna Kalinowska <hanna.kalinowska@gmail.com>
2011-03-11 19:49:17 +0000

    problem 22

---
+++ problem022.rb
@@ -0,0 +1,19 @@
+#! /usr/bin/ruby
+
+content = nil
+File.open('names.txt', 'r') do |f|
+  content = f.read
+end
+
+def name_score(name)
+  name.split('').map{|letter| letter[0] - 64}.reduce(:+)
+end
+
+names = content.split(',').map{|name| name.gsub('"', '')}.sort
+
+sum = 0
+names.each_with_index do |name, index|
+  sum += name_score(name) * (index + 1)
+end
+
+puts sum
