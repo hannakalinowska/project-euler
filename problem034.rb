596dc8ba5df457a2252c5105fe9bf78ba0baa987
Hanna Kalinowska <hanna.kalinowska@gmail.com>
2011-04-24 17:54:49 +0100

    problem 34

---
+++ problem034.rb
@@ -0,0 +1,19 @@
+#! /usr/bin/ruby
+FACTORIALS = {'0' => 1, '1' => 1, '2' => 2, '3' => 6, '4' => 24, '5' => 120, '6' => 720, '7' => 5040, '8' => 40320, '9' => 362880}
+
+def factorial(digit)
+  FACTORIALS[digit]
+end
+
+def factorial_sum(number)
+  number.to_s.chars.map {|digit| factorial(digit)}.reduce(:+)
+end
+
+sum = 0
+10.upto(9_999_999) do |number|
+  if number == factorial_sum(number)
+    sum += number
+  end
+end
+
+puts sum
