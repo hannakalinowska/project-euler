574418a3ef9be1192d856f99a444cb661b151336
Hanna Kalinowska <hanna.kalinowska@gmail.com>
2011-04-25 16:39:31 +0100

    problem 35

---
+++ problem035.rb
@@ -0,0 +1,26 @@
+#! /usr/bin/ruby
+require 'lib/prime'
+
+class Array
+  def rotate!
+    return self if self.empty?
+    number = self.shift
+    self.push(number)
+  end
+end
+
+def circular_prime?(number)
+  digits = number.to_s.split('')
+  length = digits.length
+  1.upto(length) do
+    digits.rotate!
+    return false unless digits.join('').to_i.prime?
+  end
+  true
+end
+
+count = 0
+1.upto(999_999) do |number|
+  count += 1 if circular_prime?(number)
+end
+puts count
---
+++ spec/problem035_spec.rb
@@ -0,0 +1,35 @@
+require 'spec/spec_helper'
+require 'problem035'
+
+describe Array do
+  describe "#rotate" do
+    it "puts the first element at the end of the array" do
+      [1,2,3].rotate!.should == [2,3,1]
+    end
+
+    it "returns empty array when initial array empty" do
+      [].rotate!.should == []
+    end
+
+    it "modifies an existing object" do
+      array = [1,2]
+      new_array = array.rotate!
+      array.should == [2, 1]
+      new_array.should == [2,1]
+    end
+  end
+end
+
+describe '#circular_prime?' do
+  it "returns true for circular primes" do
+    [2, 3, 5, 7, 11, 13, 17, 31, 37].each do |number|
+      circular_prime?(number).should be_true
+    end
+  end
+
+  it "returns false for non-circular primes" do
+    [4, 8, 12, 14, 15].each do |number|
+      circular_prime?(number).should be_false
+    end
+  end
+end
