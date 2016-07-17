11a18e4960b7f8f2647d4f62e4ef4b0b6ae2ad3b
Hanna Kalinowska <hanna.kalinowska@gmail.com>
2016-07-17 09:19:13 +0100

    Another attempt at problem 14

---
+++ problem014b.rb
@@ -0,0 +1,63 @@
+#!/usr/bin/env ruby
+
+require 'pry'
+
+class Collatz
+  def initialize(n, known_lengths = [])
+    @n = n
+    @known_lengths = known_lengths
+    @known_lengths[1] = 1
+  end
+
+  def sequence_length
+    if @known_lengths[@n].nil?
+      next_item = next_item(@n)
+      Collatz.new(next_item, @known_lengths).sequence_length + 1
+    else
+      @known_lengths[@n]
+    end
+  end
+
+  def sequence
+    current = @n
+    result = [current]
+    while current != 1
+      current = next_item(current)
+      result << current
+    end
+    result
+  end
+
+  private
+
+  def next_item(current)
+    if current.even?
+      current / 2
+    else
+      3 * current + 1
+    end
+  end
+end
+
+class Result
+  attr_accessor :n, :length
+
+  def initialize(n, length)
+    @n = n
+    @length = length
+  end
+end
+
+result = Result.new(0, 0)
+known_lengths = []
+
+1.upto(1_000_000) do |n|
+  length = Collatz.new(n, known_lengths).sequence_length
+  known_lengths[n] = length
+
+  if length > result.length
+    result = Result.new(n, length)
+  end
+end
+
+puts result.n, result.length
---
+++ spec/problem014b_spec.rb
@@ -0,0 +1,21 @@
+require 'rspec'
+require_relative '014'
+
+RSpec.describe Collatz do
+  describe '#sequence_length' do
+    it 'returns 10 for n=13' do
+      expect(Collatz.new(13).sequence_length).to eq(10)
+    end
+  end
+
+  describe '#sequence' do
+    it 'returns the correct list for n=1' do
+      expect(Collatz.new(1).sequence).to eq([1])
+    end
+
+    it 'returns the correct list for n=13' do
+      expected = [13, 40, 20, 10, 5, 16, 8, 4, 2, 1]
+      expect(Collatz.new(13).sequence).to eq(expected)
+    end
+  end
+end
